a83e2165e47e7e4152a6ab6acfa7fb58dfa3afc1
Felix <6337040+felixplesoianu@users.noreply.github.com>
2018-03-20 18:42:58 +0200

    Start fleshing out the INI compiler.

--- ini2json.py
+++ ini2json.py
@@ -6,6 +6,44 @@ from __future__ import print_function
 import sys
 import json
 import configparser
+import uuid
+
+def new_meta():
+	return {
+		"title": "An Interactive Fiction",
+		"author": "Anonymous",
+		"ifid": str(uuid.uuid4())
+	}
+
+def new_config():
+	return {"banner": "", "max_score": 0, "use_score": True}
+
+def new_room(name):
+	return {
+		"type": "room",
+		"name": name,
+		"description": ""
+	}
+
+def new_actor(name, loc=None):
+	return {
+		"type": "actor",
+		"name": name,
+		"description": "As good-looking as ever.",
+		"location": loc
+	}
+
+def new_game():
+	game = {
+		"meta": new_meta(),
+		"objects": {
+			"limbo": new_room("Limbo"),
+			"hero": new_actor("You", "limbo")
+		},
+		"config": new_config()
+	}
+	game["objects"]["limbo"]["description"] = "You are in limbo."
+	return game
 
 def parse_value(text):
 	low = text.lower()
@@ -15,41 +53,43 @@ def parse_value(text):
 		return False
 	else:
 		try:
-			return float(text)
+			return int(text)
 		except ValueError:
 			return text
 		except OverflowError:
 			return text
 
+def config2game(config):
+	output = new_game()
+
+	for i in config["META"]:
+		output["meta"][i] = config["META"][i]
+	for i in config["CONFIG"]:
+		output["config"][i] = parse_value(config["CONFIG"][i])
+	for i in config:
+		if i in ["DEFAULT", "CONFIG", "META"]:
+			continue
+
+		if i not in output["objects"]:
+			output["objects"][i] = {}
+		obj = config[i]
+		for j in obj:
+			output["objects"][i][j] = parse_value(obj[j])
+	
+	return output
+
 if len(sys.argv) < 2:
 	print("Usage: ini2json.py <input file>")
 	exit()
 
-game = configparser.ConfigParser()
+config = configparser.ConfigParser()
 
 try:
 	with open(sys.argv[1], "r") as f:
-		game.read_file(f)
+		config.read_file(f)
 		# TO DO: sanity checks
 except Exception as e:
 	print("Couldn't read INI file: " + str(e))
 
-output = {}
-
-output["meta"] = {}
-for i in game["META"]:
-	output["meta"][i] = game["META"][i]
-output["config"] = {}
-for i in game["CONFIG"]:
-	output["config"][i] = parse_value(game["CONFIG"][i])
-output["objects"] = {}
-for i in game:
-	if i in ["DEFAULT", "CONFIG", "META"]:
-		continue
-	else:
-		output["objects"][i] = {}
-		obj = game[i]
-		for j in obj:
-			output["objects"][i][j] = parse_value(obj[j])
-
+output = config2game(config)
 print(json.dumps(output))
